f806f4f8b3a96498d2818b0ea044e2334eff1fc7
Mang <mangemmanuel07@gmail.com>
2023-03-21 00:16:26 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "birthday";
+const messages = writeCards(names, event);
+console.log(messages);
+
+
+function countDown() {
+  let countDownNum = 11;
+    while (countDownNum--) {
+      console.log(countDownNum)
+    }
+
+}
